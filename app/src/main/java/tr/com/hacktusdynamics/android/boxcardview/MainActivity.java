28f5d78842200ec1f7636d4bf9a53b0af8969777
svktylmz <sevketyilmaz@iyte.edu.tr>
2016-02-01 21:58:29 +0200

    btn clicks tested.

--- app/src/main/java/tr/com/hacktusdynamics/android/boxcardview/MainActivity.java
+++ app/src/main/java/tr/com/hacktusdynamics/android/boxcardview/MainActivity.java
@@ -2,6 +2,10 @@ package tr.com.hacktusdynamics.android.boxcardview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import tr.com.hacktusdynamics.android.boxcardview.view.BoxCardView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BoxCardView cardView1 = (BoxCardView) findViewById(R.id.cardView1);
+        cardView1.setOnNormalButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Card1 normal btn clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final BoxCardView cardView2 =(BoxCardView) findViewById(R.id.cardView2);
+        cardView2.setOnHighlightButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Card2 Highlight btn clicked", Toast.LENGTH_SHORT).show();
+                cardView2.setTitle("Highlight Title");
+            }
+        });
     }
 }
