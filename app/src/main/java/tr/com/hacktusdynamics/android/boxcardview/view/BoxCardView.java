322e064bdbd7fef0d33fe8db69f0b6baa419ad41
svktylmz <sevketyilmaz@iyte.edu.tr>
2016-02-01 21:02:41 +0200

    inflate and refresh.

--- app/src/main/java/tr/com/hacktusdynamics/android/boxcardview/view/BoxCardView.java
+++ app/src/main/java/tr/com/hacktusdynamics/android/boxcardview/view/BoxCardView.java
@@ -4,8 +4,16 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.CardView;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import tr.com.hacktusdynamics.android.boxcardview.R;
 
@@ -43,6 +51,30 @@ public class BoxCardView extends CardView{
             a.recycle();
         }
 
+        setRadius((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, getResources().getDisplayMetrics()));
+        setPreventCornerOverlap(false);
+        setCardBackgroundColor(context.getResources().getColor(R.color.box_cardview_background));
 
+        inflate();
     }
+
+    private void inflate() {
+        removeAllViews();
+        LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        layoutInflater.inflate(R.layout.box_card_view, this, true);
+
+        refresh();
+    }
+
+    private void refresh() {
+        LinearLayout layoutAll = (LinearLayout) getChildAt(0);
+        handleImage(layoutAll, 0);
+
+        LinearLayout layout = (LinearLayout) layoutAll.getChildAt(1);
+        handleTitle(layout, 0);
+        handleDescription(layout, 1);
+        handleSpacing(layout, 2);
+        handleCardViewNormalButtons(layout, 3);
+    }
+
 }
